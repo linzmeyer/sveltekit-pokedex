d1c5ee2edecd7fdf278e39bedbb3018a003b360c
context-coding <context.coding@gmail.com>
2021-11-13 02:05:52 -0600

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
+import node from '@sveltejs/adapter-node';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: node()
+		adapter: {
+			adapt: async () => await node()
+		},
 	}
 };
 
