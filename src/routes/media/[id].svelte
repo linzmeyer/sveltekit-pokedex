3e38bce37e38f6b0dab9e5c706e1ae3dbfdd2fea
context-coding <context.coding@gmail.com>
2021-11-15 00:39:10 -0600

    update

--- src/routes/media/[id].svelte
+++ src/routes/media/[id].svelte
@@ -4,11 +4,11 @@
 
 
 <div class="flex flex-col items-center">
-  <img class="card-image logo-img1" src={`${envars.PATH_TO_IMAGES}/context_logo1.png`} alt="context coding logo" />
+  <!-- <img class="card-image logo-img1" src={`${envars.PATH_TO_IMAGES}/context_logo1.png`} alt="context coding logo" /> -->
 </div>
 
 <div>
-  <iframe height="200" src={`${envars.PATH_TO_VIDEOS}/multi-cursor.mp4`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" class="vid-test" allowfullscreen></iframe>
+  <!-- <iframe height="200" src={`${envars.PATH_TO_VIDEOS}/multi-cursor.mp4`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" class="vid-test" allowfullscreen></iframe> -->
 </div>
 
 <style>
