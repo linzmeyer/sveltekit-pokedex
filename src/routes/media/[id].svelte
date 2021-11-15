116050fa1f3b71316beee329d54f5ab30411c8dc
context-coding <context.coding@gmail.com>
2021-11-15 00:10:31 -0600

    update

---
+++ src/routes/media/[id].svelte
@@ -0,0 +1,36 @@
+<script>
+  import { envars } from '../../envars-public';
+</script>
+
+
+<div class="flex flex-col items-center">
+  <img class="card-image logo-img1" src={`${envars.PATH_TO_IMAGES}/context_logo1.png`} alt="context coding logo" />
+</div>
+
+<div>
+  <iframe height="200" src={`${envars.PATH_TO_VIDEOS}/multi-cursor.mp4`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" class="vid-test" allowfullscreen></iframe>
+</div>
+
+<style>
+@media screen and (min-width: 0px) and (max-width: 400px) {
+  .logo-img1 {
+    height: 100px;
+  }
+}
+@media screen and (min-width: 401px) and (max-width: 700px) {
+  .logo-img1 {
+    height: 500px;
+  }
+}
+@media screen and (min-width: 701px) {
+  .logo-img1 {
+    height: 800px;
+  }
+}
+
+.vid-test {
+  margin-top: 20px;
+  border-radius: 30px;
+}
+
+</style>
